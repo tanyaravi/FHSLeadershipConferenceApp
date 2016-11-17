5cff1faabd5bd6fbff94b8f20d2c0bcd99622e1a
Tanya <tanya@administrators-MacBook-Pro.local>
2016-11-17 08:37:14 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tanya/fhsleadershipconferenceapp/FHSMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.tanya.fhsleadershipconferenceapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FHSMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fhsmain);
+    }
+}
